a08516cc8890d534d2572a5e18838ecad3ae0938
AlexParreira <alexander.o.parreira@gmail.com>
2023-08-09 18:22:38 -0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Alexander Parreira on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
