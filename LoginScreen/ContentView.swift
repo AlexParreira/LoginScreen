21ca381eb43a1916176c152402d0b86d72682c04
AlexParreira <alexander.o.parreira@gmail.com>
2023-08-09 19:25:40 -0300

    first

--- LoginScreen/ContentView.swift
+++ LoginScreen/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var vm = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if vm.authenticated{
+            VStack{
+                Text("Welcome back **\(vm.username.lowercased())**!")
+                Text("Today is: ***\(Date().formatted(.dateTime))*")
+                Button("Log out", action: vm.logOut)
+                    .tint(.red)
+                    .buttonStyle(.bordered)
+            }
+        } else {
+            ZStack{
+                Image("StarsAndMoon")
+                    .resizable()
+                    .cornerRadius(20)
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .leading, spacing: 20){
+                    Spacer()
+                    Text("Log in")
+                        .foregroundColor(.white)
+                        .font(.system(size: 50, weight: .medium, design: .rounded))
+                        
+                    TextField("Username", text: $vm.username)
+                        .textFieldStyle(.roundedBorder)
+                        .textInputAutocapitalization(.never)
+                    SecureField("Password", text: $vm.password)
+                        .textFieldStyle(.roundedBorder)
+                        .textInputAutocapitalization(.never)
+                        .privacySensitive()
+                    HStack{
+                        Spacer()
+                        Button("Forgot password?", action: vm.logPressed )
+                            .tint(.black.opacity(0.8))
+                        Spacer()
+                        Button("Log in", action: vm.authenticate)
+                            .buttonStyle(.bordered)
+                            .tint(.white)
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                .alert("Access denied", isPresented: $vm.invalid){
+                    Button("Dismiss", action: vm.logPressed)
+                }
+                .frame(width: 300)
+                .padding()
+            }
+            .transition(.offset(x:0, y:850))
         }
-        .padding()
     }
 }
 
